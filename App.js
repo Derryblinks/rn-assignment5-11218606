fb1c0be8e8aa8c885f9bc3e26ca695de34b80057
Derryblinks <db723252@gmail.com>
2024-06-24 23:10:22 +0000

    Updated code with components and files

--- App.js
+++ App.js
@@ -1,39 +1,11 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-
-// App.js
 import React from 'react';
-import { AppearanceProvider } from 'react-native-appearance';
-import { ThemeProvider } from './theme/ThemeContext';
-import Navigation from './Navigation';
+import TabNavigator from './navigation/TabNavigator';
+import { ThemeProvider } from './context/ThemeContext';
 
-const App = () => {
+export default function App() {
   return (
-    <AppearanceProvider>
-      <ThemeProvider>
-        <Navigation />
-      </ThemeProvider>
-    </AppearanceProvider>
+    <ThemeProvider>
+      <TabNavigator />
+    </ThemeProvider>
   );
-};
-
-export default App;
+}
